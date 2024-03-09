55638c837057c3da4f3c97c56eff770f1053ec1a
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-03-09 18:31:54 +1100

    Update schema

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ const config = {
 		'@typescript-eslint/require-await': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
 		'@typescript-eslint/no-unsafe-return': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/array-type': 'off',
 		'@typescript-eslint/consistent-type-definitions': 'off',
 		'@typescript-eslint/no-empty-interface': 'off',
--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,121 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { relations, sql } from 'drizzle-orm'
+import {
+	integer,
+	pgEnum,
+	pgTable,
+	text,
+	timestamp,
+	uuid,
+	varchar
+} from 'drizzle-orm/pg-core'
+import { v4 as uuidv4 } from 'uuid'
 
+export const exerciseCategory = pgEnum('exercise_category', [
+	'distance',
+	'sport',
+	'reps'
+])
+
+const createdAndUpdated = {
+	createdAt: timestamp('created_at')
+		.default(sql`now()`)
+		.notNull(),
+	updatedAt: timestamp('updated_at')
+		.default(sql`now()`)
+		.notNull()
+}
+
+/** Table for storing extra data on users. The id matches the auth id */
 export const users = pgTable('users', {
 	id: uuid('id').primaryKey(),
 	fullName: text('full_name')
 })
+
+/** Table for storing a single exercise event, like swimming 50 laps of 25m pool  */
+export const exerciseEvents = pgTable('exercise_events', {
+	id: uuid('id')
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => uuidv4()),
+	date: timestamp('date').$defaultFn(() => new Date()),
+	notes: text('notes'),
+	exerciseCategory: exerciseCategory('exercise_category'),
+	userId: varchar('userId', { length: 36 })
+		.notNull()
+		.references(() => users.id)
+})
+
+/** Table for storing an exercise session, like a gym ab workout session */
+export const exerciseSessions = pgTable('exercise_sessions', {
+	id: uuid('id')
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => uuidv4()),
+	date: timestamp('date').$defaultFn(() => new Date()),
+	notes: text('notes'),
+	userId: varchar('userId', { length: 36 })
+		.notNull()
+		.references(() => users.id)
+})
+
+/** Table for storing each set in an exercise session */
+export const exerciseSets = pgTable('exercise_sets', {
+	id: uuid('id')
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => uuidv4()),
+	reps: integer('reps'),
+	sets: integer('sets').default(1),
+	// By default it will be an event unless an exercise session is created
+	exerciseEventId: varchar('exercise_event_id', { length: 255 }).references(
+		() => exerciseSessions.id
+	),
+	exerciseSessionId: varchar('exercise_session_id', { length: 255 }).references(
+		() => exerciseSessions.id
+	),
+	exerciseTitle: varchar('exercise_title', { length: 255 }).notNull(),
+	...createdAndUpdated
+})
+
+/**
+ * RELATIONS
+ */
+
+/** An exercise session will be tied to one user but can have many sets */
+export const exerciseSessionsRelations = relations(
+	exerciseSessions,
+	({ many, one }) => ({
+		exerciseSets: many(exerciseSets),
+		user: one(users, {
+			fields: [exerciseSessions.userId],
+			references: [users.id]
+		})
+	})
+)
+
+/** An exercise session will be tied to one user and one exercise set */
+export const exerciseEventsRelations = relations(exerciseEvents, ({ one }) => ({
+	exerciseSets: one(exerciseSets),
+	user: one(users, {
+		fields: [exerciseEvents.userId],
+		references: [users.id]
+	})
+}))
+
+/** An exercise set will always be linked to one exercise */
+export const exerciseSetsRelations = relations(exerciseSets, ({ one }) => ({
+	exerciseEvent: one(exerciseEvents, {
+		fields: [exerciseSets.exerciseSessionId],
+		references: [exerciseEvents.id]
+	}),
+	exerciseSessions: one(exerciseSessions, {
+		fields: [exerciseSets.exerciseSessionId],
+		references: [exerciseSessions.id]
+	})
+}))
+
+/** User can have many exercise events and sessions */
+export const usersRelations = relations(users, ({ many }) => ({
+	exerciseEvents: many(exerciseSessions),
+	exerciseSessions: many(exerciseSessions)
+}))
